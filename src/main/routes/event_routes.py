9d7db11f84cda39c4dab6f5d9a2d22873a359c8a
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-04-04 11:48:59 -0400

    Implements add events route

--- src/main/routes/event_routes.py
+++ src/main/routes/event_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 
 from src.http_types.http_request import HttpRequest
+from src.data.event_handler import EventHandler
 
 event_route_bp = Blueprint("event_route", __name__)
 
@@ -8,4 +9,8 @@ event_route_bp = Blueprint("event_route", __name__)
 def create_event():
   http_request = HttpRequest(body=request.json)
   
-  return jsonify({ "message": "Hello World" }), 200
+  event_handler = EventHandler()
+
+  http_response = event_handler.register(http_request)
+
+  return jsonify(http_response.body), http_response.status_code
--- src/main/server/server.py
+++ src/main/server/server.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_cors import CORS
 
 from src.main.routes.event_routes import event_route_bp
+from src.models.settings.connection import db_connection_handle
+
+db_connection_handle.connect_to_db()
 
 app = Flask(__name__)
 CORS(app)
