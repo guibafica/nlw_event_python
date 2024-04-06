8349f96e31aaa3a393c54a963d8d2294ad46afa0
Guilherme Bafica <gb.engcomp@hotmail.com>
2024-04-05 20:11:44 -0400

    Create get event method and rote

--- src/data/event_handler.py
+++ src/data/event_handler.py
@@ -18,3 +18,22 @@ class EventHandler:
       body={ "eventId": body["uuid"] },
       status_code=200
     )
+
+  def find_by_id(self, http_request: HttpRequest) -> HttpResponse:
+    event_id = http_request.param["event_id"]
+    event = self.__events_repository.get_event_by_id(event_id)
+
+    if not event: raise Exception("No event found with this id")
+
+    return HttpResponse(
+      body={
+        "event": {
+          "id": event.id,
+          "title": event.title,
+          "detail": event.details,
+          "slug": event.slug,
+          "maximumAttendees": event.maximum_attendees,
+        }
+      },
+      status_code=200
+    )
